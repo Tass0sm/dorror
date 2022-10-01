6271f18fe576be6922f3ecda61ec71c6b08ae203
Tassos Manganaris <tassos.manganaris@gmail.com>
2022-09-30 20:51:19 -0400

    Added a few models and a little game logic

--- Conflict.gd
+++ Conflict.gd
@@ -1,15 +1,8 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -17,5 +10,10 @@ func _process(delta):
 		$Door.close()
 	if Input.is_action_pressed("push_right"):
 		$Door.open()
+		
+	if $Door.is_closed:
+		print("closer wins")
+	if $Door.is_opened:
+		print("opener wins")
 	
 	pass
--- Door.gd
+++ Door.gd
@@ -1,17 +1,19 @@
 extends CSGBox
 
-
 # Declare member variables here. Examples:
 var open_level = 0
-# var b = "text"
-
+var open_min = 0
+var open_max = 10
+var is_opened = false
+var is_closed = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	open_level = 5
 	pass # Replace with function body.
 
 func open():
-	if open_level < 5:
+	if open_level < open_max:
 		open_level += 1
 
 func close():
@@ -20,4 +22,11 @@ func close():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if open_level == open_min:
+		is_opened = false
+		is_closed = true
+	if open_level == open_max:
+		is_opened = true
+		is_closed = false
+		
 	pass
